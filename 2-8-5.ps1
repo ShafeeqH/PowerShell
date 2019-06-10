f1d10fcb8d908cd8287f8e0d2691e5d803213db8
Shafeeq Hameed <33703534+ShafeeqH@users.noreply.github.com>
2019-06-10 17:06:42 -0500

    Update 2-8-5.ps1

---
+++ 2-8-5.ps1
@@ -0,0 +1,23 @@
+<#
+
+Author: Shafeeq Hameed
+Current Date: June 10 2019 
+
+#>
+$CEREALS = @("cornflakes", "raisin bran", "Special K","Captian Krunch","Frosted Flakes","Count Chocola")
+$CEREALS = $CEREALS | Where-Object {$_ -ne 'cornflakes'}
+$CEREALS += "CoCo Puffs"
+$CEREALS
+
+$CEREALS = New-Object 'Collections.Generic.List[string]'
+$CEREALS.Add("cornflakes")
+$CEREALS.Add("raisin bran")
+$CEREALS
+
+
+$FilesList= Get-ChildItem -Path "C:\Resources" -Recurse
+$Array = @()
+    foreach ($file in $FilesList){$Array += $file}
+$Array
+
+
