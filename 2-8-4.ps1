8a62b2cbff5bc07f30bb8f2ce1e0cb9a30ad6503
Shafeeq Hameed <33703534+ShafeeqH@users.noreply.github.com>
2019-06-10 16:36:27 -0500

    Lessons

---
+++ 2-8-4.ps1
@@ -0,0 +1,23 @@
+<#
+
+Author: Shafeeq Hameed
+Current Date: June 10 2019 
+
+#>
+$Example1 = "A string of Text"
+$Example1.Substring($($Example1.IndexOf("Text")),4) 
+$String1="29"
+$String2="Your instructior is only"
+$String3= "$($String2) $($String1)"
+$String3
+
+$StartingString = "The house is gray and the sky is blue"
+<# $String4= "$($StartingString.Substring(0,13))blue $($StartingString.Substring(18,15))gray" #>
+<#$String4 ="$($StartingString.Substring(0,13))$($StartingString.Substring($StartingString.Length -4))$($StartingString.Substring(17,16))$($StartingString.Substring(13,5))" #>
+$String4 = $StartingString.Replace("gray","1")
+$String4 = $String4.Replace("blue","2")
+$String4 = $String4.Replace("1","blue")
+$String4 = $String4.Replace("2","gray")
+
+
+$String4
