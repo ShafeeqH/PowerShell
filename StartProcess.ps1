a37fe17d4b8f625c2f73620a1365190eed70e0fb
Shafeeq Hameed <33703534+ShafeeqH@users.noreply.github.com>
2019-06-24 09:33:48 -0400

    Create StartProcess.ps1

---
+++ StartProcess.ps1
@@ -0,0 +1,7 @@
+ Start-Process -FilePath "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" -ArgumentList "/update user","updatepromptuser=true", "forceappshutdown=true","displaylevel=true"
+
+
+
+
+
+
